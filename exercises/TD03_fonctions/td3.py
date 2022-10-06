25791d936007628edbeeee148d52d52ae188fd03
Hadj <hadj.diallo@ens.uvsq.fr>
2022-10-06 10:59:26 +0200

    FIN TD 1 P2

---
+++ exercises/TD03_fonctions/td3.py
@@ -0,0 +1,11 @@
+def TempsEnSeconde(temps):
+    """Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
+    pass 
+
+temps= (3, 23, 1, 34)
+print(type(temps))
+print(tempsEnSeconde(temps))
+
+def secondeEnTemps(second)
+
+
